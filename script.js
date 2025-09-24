09c162a69f4762ab35c81a59e350e69cddbb9ab1
ynnaual <lauanny.lima@escola.pr.gov.br>
2025-09-24 11:34:41 +0000

    aula 7

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-avanca.forEach(button =>{//para cada clique ele avanca
-   button.addEventListener(click,function(){
-const atual = document.querySelector('.ativo')
-const proximoPasso = 'passo-0'+this.getAttribute('data-proximo');
 
-atual.classList.remove('ativo');
-document.getElementById(proximoPasso).classList.add('ativo');
-   })
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-}); 
-
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
